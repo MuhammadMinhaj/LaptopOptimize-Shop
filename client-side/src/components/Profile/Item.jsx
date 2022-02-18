dad8f248dfb943c2830d849d5d4b58e2e6d8957e
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2022-02-18 22:46:36 +0600

    feat: Changed to improved UI

--- client-side/src/components/Profile/Item.jsx
+++ client-side/src/components/Profile/Item.jsx
@@ -5,26 +5,30 @@ function Item({ name, icon, path, handleClick }) {
   const handleClickChangeRoute = () => history.push(path);
   return (
     <Grid item xs={12} sm={6} md={4}>
-      <Box
-        bgcolor="var(--primary)"
-        boxShadow={2}
-        borderRadius="0.5rem"
-        overflow="hidden"
-      >
+      <Box boxShadow={3} borderRadius="0.75rem" overflow="hidden">
         <CardActionArea onClick={handleClick || handleClickChangeRoute}>
-          <Box p="1rem">
-            <Box
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Box color="var(--secondary)" fontSize="3rem">
+          <Box>
+            <Box display="flex" justifyContent="space-between">
+              <Box
+                color="var(--white)"
+                fontSize="3rem"
+                bgcolor="var(--primary-deep)"
+                p="1.5rem"
+                borderRadius="0rem 2rem 2rem 0rem"
+              >
                 {icon}
               </Box>
-              <Typography variant="h5" color="var(--white)">
-                {name}
-              </Typography>
+              <Box
+                bgcolor="var(--white)"
+                width="100%"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Typography variant="h6" color="var(--primary)">
+                  {name}
+                </Typography>
+              </Box>
             </Box>
           </Box>
         </CardActionArea>
